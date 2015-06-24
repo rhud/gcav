5e519eafd06c728ea9ad78e230fd05bb54801ba2
rhud <ryan@hellohudson.com.au>
2015-06-25 01:16:09 +1000

    jess' changes

--- templates/content-page.php
+++ templates/content-page.php
@@ -3,22 +3,26 @@
 	<?php
 		global $post;
 		    $post_slug=$post->post_name;
+		    $heroImg = types_render_field("hero-img", array("raw"=>"true"));
+		    if($heroImg == "") {
+		    	$heroImg = types_render_field("page-img", array("raw"=>"true"));
+		    }
 	?>
-	<header class="<?=$post_slug;?>">
+	<header class="<?=$post_slug;?>" style="background-image:url(<?=$heroImg;?>);">
 	  	<div class="inner">
 	  		<?php get_template_part('templates/page', 'header'); ?>
-		</div>
-	</header>
-	
-	<div class="page inner">
-		<div class="content">
-			<?php 
+	  		<?php 
 				$hero = types_render_field("hero-msg", array("raw"=>"true"));
 				if(!is_null($hero)) {
 					?><h2 class="hero"><?=$hero;?></h2><?
 				}
 			?>
-		  	<?php the_content(); ?>
+		</div>
+	</header>
+	
+	<div class="page inner">
+		<div class="content">
+			<?php the_content(); ?>
 		</div>
 	</div>
 
